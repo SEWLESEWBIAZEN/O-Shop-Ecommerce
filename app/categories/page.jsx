09ce4f91d99e974e09a5f82baccd1aef8737c7f6
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-05 11:12:18 +0300

    blank space

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Categories from '../_components/Categories'
-import {AiOutlineArrowRight} from 'react-icons/ai'
-
-
+import { AiOutlineArrowRight } from 'react-icons/ai'
+import React from 'react'
 
 export const metadata = {
   title: "O-Shop | Select Your Categories",
-  icons:{
-    icon:'/favicon.ico',
+  icons: {
+    icon: '/favicon.ico',
   },
   description: "An Online shop that you can order the product you want to purchase, and it is an online show room. Find bunch of categories from which your best is included",
 };
@@ -19,13 +18,13 @@ const Page = () => {
     <div className='py-10 '>
       <div className='container'>
         <div className='flex flex-row justify-between'>
-        <h1 className='font-semibold'>TOP RATED CATEGORIES</h1>
-        <div className='flex flex-row place-items-center text-gray-400 gap-2 cursor-pointer hover:text-[var(--primary-color)]'>
-          <h1 className='font-semibold '>See All </h1>
-          <AiOutlineArrowRight
-          size={20}
-           className='hover:w-6 hover:h-6 '/> 
-        </div>
+          <h1 className='font-semibold'>TOP RATED CATEGORIES</h1>
+          <div className='flex flex-row place-items-center text-gray-400 gap-2 cursor-pointer hover:text-[var(--primary-color)]'>
+            <h1 className='font-semibold '>See All </h1>
+            <AiOutlineArrowRight
+              size={20}
+              className='hover:w-6 hover:h-6 ' />
+          </div>
         </div>
         <div className='text-center '>
           <Categories />
