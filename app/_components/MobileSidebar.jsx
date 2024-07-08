e60857eaf1c190679f2c3f60929f0d15dc72c90f
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 09:37:37 +0300

    styling mobile side navigation bar

--- app/_components/MobileSidebar.jsx
+++ app/_components/MobileSidebar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const MobileSidebar = () => {
     return (
-        <div className='rounded-lg bg-gray-200 gap-3 grid absolute bottom-[3.2rem] text-start left-[35%] z-50 px-2 py-4'>
-            <Link href="/categories" className='bg-gray-100 px-6 text-black text-[14px]'>CATEGORIES</Link>
-            <Link href="/men" className='bg-gray-100  px-6 text-black text-[14px]'>MEN</Link>
-            <Link href="/women" className='bg-gray-100  px-6 text-black text-[14px]'>WOMEN</Link>
-            <Link href="/jewlery" className='bg-gray-100  px-6 text-black text-[14px]'>JEWLERY</Link>
-            <Link href="/perfume" className='bg-gray-100  px-6 text-black text-[14px]'>PERFUME</Link>
-            <Link href="/blog" className='bg-gray-100  px-6 text-black text-[14px]'>BLOR</Link>
-            <Link href="/hot-offers" className='bg-gray-100  px-6 text-black text-[14px]'>HOT OFFERS</Link>
+        <div className='rounded-lg bg-gray-200 gap-2 grid absolute bottom-[3.2rem] text-start left-[35%] z-50 px-2 py-4'>
+            <Link href="/categories" className=' px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>CATEGORIES</Link>
+            <Link href="/men" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>MEN</Link>
+            <Link href="/women" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>WOMEN</Link>
+            <Link href="/jewlery" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>JEWLERY</Link>
+            <Link href="/perfume" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>PERFUME</Link>
+            <Link href="/blog" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>BLOR</Link>
+            <Link href="/hot-offers" className='  px-6 py-1 text-black font-semibold hover:bg-[var(--primary-color)] rounded-sm text-[14px]'>HOT OFFERS</Link>
         </div>
     )
 }
