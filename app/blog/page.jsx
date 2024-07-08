a32297c26ccf0e4499022763e280ecf62c99de1b
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 12:10:38 +0300

    creating blog page

---
+++ app/blog/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <div className='h-screen flex justify-center items-center'>No BLOG Available</div>
+    )
+}
+
+export default BlogPage
