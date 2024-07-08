7ddef2361d03d57fcd281d6897a27118e9f796f6
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 08:24:19 +0300

    width auto top header

--- app/_components/HeaderTop.jsx
+++ app/_components/HeaderTop.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'
 const Page = () => {
   return (
     <div className=' border-b border-gray-300'>
-      <div className='mx-5 w-full p-3 flex flex-row justify-center gap-20 items-center'>
+      <div className='mx-5 w-auo p-3 flex flex-row justify-center gap-20 items-center'>
         <div className='hidden md:flex md:flex-row gap-2 '>
           <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaFacebook /></div>
           <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaTwitter /></div>
@@ -15,7 +15,7 @@ const Page = () => {
           <strong>FREE SHIPPING </strong>
           THIS WEEK ORDER OVER - $55
         </div>
-        <div className='flex flex-col sm:flex-row gap-2 pe-2'>
+        <div className='flex flex-col sm:flex-row gap-2 pe-1'>
           <select name='currency' id="currency" className=' p-1 border border-gray-200 text-[10px] md:text-[12px] text-gray-400'>
             <option value="ETB">ETB</option>
             <option value="$USD">$USD</option>
