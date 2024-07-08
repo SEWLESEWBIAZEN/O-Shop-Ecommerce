73a678a43343ed86bc52b550a3c3ee8aaa0cebd0
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 08:21:04 +0300

    adding padding end to currency and language dropdowns

--- app/_components/HeaderTop.jsx
+++ app/_components/HeaderTop.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { FaFacebook,FaTwitter, FaInstagram,FaLinkedin } from 'react-icons/fa'
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 const Page = () => {
   return (
     <div className=' border-b border-gray-300'>
-        <div className='mx-5 w-full p-3 flex flex-row justify-center gap-20 items-center'>
+      <div className='mx-5 w-full p-3 flex flex-row justify-center gap-20 items-center'>
         <div className='hidden md:flex md:flex-row gap-2 '>
-            <div className='social-media-link bg-gray-400 p-1 rounded rounded-md'><FaFacebook/></div>
-            <div className='social-media-link bg-gray-400 p-1 rounded rounded-md'><FaTwitter/></div>
-            <div className='social-media-link bg-gray-400 p-1 rounded rounded-md'><FaInstagram/></div>
-            <div className='social-media-link bg-gray-400 p-1 rounded rounded-md'><FaLinkedin/></div>
+          <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaFacebook /></div>
+          <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaTwitter /></div>
+          <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaInstagram /></div>
+          <div className='social-media-link bg-gray-400 p-1 rounded-md'><FaLinkedin /></div>
         </div>
         <div className="text-gray-500 text-[10px] md:text-[12px]">
-            <strong>FREE SHIPPING </strong>
-            THIS WEEK ORDER OVER - $55
-          </div>
-        <div className='flex flex-col sm:flex-row gap-2'>
+          <strong>FREE SHIPPING </strong>
+          THIS WEEK ORDER OVER - $55
+        </div>
+        <div className='flex flex-col sm:flex-row gap-2 pe-2'>
           <select name='currency' id="currency" className=' p-1 border border-gray-200 text-[10px] md:text-[12px] text-gray-400'>
             <option value="ETB">ETB</option>
             <option value="$USD">$USD</option>
@@ -27,7 +27,7 @@ const Page = () => {
             <option value="French">French</option>
           </select>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
