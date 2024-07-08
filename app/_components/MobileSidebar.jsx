afc8e87e7c647262016495b044d27e32a266a97c
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 09:24:33 +0300

    adding link functionality to mobile side nav

--- app/_components/MobileSidebar.jsx
+++ app/_components/MobileSidebar.jsx
@@ -1,16 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const MobileSidebar = () => {
     return (
-        <div className='rounded-lg bg-[var(--primary-color)] gap-3 grid absolute bottom-[10rem] text-center left-2 z-50 px-2 py-4'>
-            <div className='bg-gray-200 bg-transparent-md rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
-            <div className='bg-gray-50 rounded-md px-6 text-black text-[14px]'>link</div>
+        <div className='rounded-lg bg-gray-200 gap-3 grid absolute bottom-[10rem] text-start left-2 z-50 px-2 py-4'>
+            <Link href="/categories" className='bg-gray-100 px-6 text-black text-[14px]'>CATEGORIES</Link>
+            <Link href="/men" className='bg-gray-100  px-6 text-black text-[14px]'>MEN</Link>
+            <Link href="/women" className='bg-gray-100  px-6 text-black text-[14px]'>WOMEN</Link>
+            <Link href="/jewlery" className='bg-gray-100  px-6 text-black text-[14px]'>JEWLERY</Link>
+            <Link href="/perfume" className='bg-gray-100  px-6 text-black text-[14px]'>PERFUME</Link>
+            <Link href="/blog" className='bg-gray-100  px-6 text-black text-[14px]'>BLOR</Link>
+            <Link href="/hot-offers" className='bg-gray-100  px-6 text-black text-[14px]'>HOT OFFERS</Link>
         </div>
     )
 }
