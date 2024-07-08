dbef99b9e38f1c51b6c4ba021e6a8e0188eef710
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 09:27:09 +0300

    placing at the center links

--- app/_components/MobileSidebar.jsx
+++ app/_components/MobileSidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const MobileSidebar = () => {
     return (
-        <div className='rounded-lg bg-gray-200 gap-3 grid absolute bottom-[10rem] text-start left-2 z-50 px-2 py-4'>
+        <div className='rounded-lg bg-gray-200 gap-3 grid absolute bottom-[3.2rem] text-start left-[35%] z-50 px-2 py-4'>
             <Link href="/categories" className='bg-gray-100 px-6 text-black text-[14px]'>CATEGORIES</Link>
             <Link href="/men" className='bg-gray-100  px-6 text-black text-[14px]'>MEN</Link>
             <Link href="/women" className='bg-gray-100  px-6 text-black text-[14px]'>WOMEN</Link>
