cbbfb95d0222afee0ce46628b0d40a3d977ceb49
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 10:05:21 +0300

    creating an array of links and iterating through it instead of writing  the same class over and over again

--- app/_components/MobileSidebar.jsx
+++ app/_components/MobileSidebar.jsx
@@ -1,16 +1,47 @@
 import Link from 'next/link'
 import React from 'react'
+//MEN", "WOMEN", "JEWLERY", "PERFUME", "BLOG", "HOT OFFERS"}
+const links =
+    [
+        {
+            link: "CATEGORIES",
+            href: "/categories"
+        },
+        {
+            link: "MEN",
+            href: "/men"
+        },
+        {
+            link: "WOMEN",
+            href: "/women"
+        },
+        {
+            link: "JEWLERY",
+            href: "/jewlery"
+        },
+        {
+            link: "PERFUME",
+            href: "/perfume"
+        },
+        {
+            link: "BLOG",
+            href: "/blog"
+        },
+        {
+            link: "HOT OFFERS",
+            href: "/hot-offers"
+        },
+    ]
+
+
 
 const MobileSidebar = () => {
     return (
         <div className='rounded-lg bg-gray-200 gap-2 grid absolute bottom-[3.2rem] text-start left-[35%] z-50 px-2 py-4'>
-            <Link href="/categories" className=' px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>CATEGORIES</Link>
-            <Link href="/men" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>MEN</Link>
-            <Link href="/women" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>WOMEN</Link>
-            <Link href="/jewlery" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>JEWLERY</Link>
-            <Link href="/perfume" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>PERFUME</Link>
-            <Link href="/blog" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>BLOG</Link>
-            <Link href="/hot-offers" className='  px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>HOT OFFERS</Link>
+            {links.map((link) => {
+                return (<Link href={link.href} className=' px-6 py-1 text-black font-semibold hover:text-[var(--primary-color)] rounded-sm text-[14px]'>{link.link}</Link>)
+            })}
+
         </div>
     )
 }
