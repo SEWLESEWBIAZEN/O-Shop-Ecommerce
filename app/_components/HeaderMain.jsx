98dd7a18e741b28798ff9a44c575ff23153ed7c4
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 08:40:50 +0300

    duplicate classes

--- app/_components/HeaderMain.jsx
+++ app/_components/HeaderMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BiSolidShoppingBag } from 'react-icons/bi'
-import {BsSearch,BsHeart} from 'react-icons/bs'
+import { BsSearch, BsHeart } from 'react-icons/bs'
 import { FiUser } from 'react-icons/fi'
 
 
@@ -11,31 +11,31 @@ const Page = () => {
         <div className='text-[20px] hidden sm:block'><b>O-shop</b></div>
         <div className='w-full sm:w-[300px] md:w-[70%] relative flex flex-row items-center -space-x-7'>
           <input
-          type='text'
-          className='w-full border border-gray-200 p-2 px-4 rounded-lg '
-          placeholder='Enter any product name....'
+            type='text'
+            className='w-full border border-gray-200 p-2 px-4 rounded-lg '
+            placeholder='Enter any product name....'
           />
-          <BsSearch 
-          size={25}
-          className='text-gray-400 icon'/>
+          <BsSearch
+            size={25}
+            className='text-gray-400 icon' />
         </div>
         <div className='hidden sm:flex  gap-3 '>
           <div>
-            <FiUser  className='w-6 h-6 icon'/>
-            </div>
-          <div className='flex flex-col items-center -space-y-11 '>
-            <BsHeart className='w-6 h-6 icon'/>
-            <div className='px-[6px] rounded rounded-full z-40 grid place-items-center bg-[var(--primary-color)] text-white rounded rounded-[4px] text-[#f45f3f] text-bold'><b>4</b></div>
+            <FiUser className='w-6 h-6 icon' />
           </div>
           <div className='flex flex-col items-center -space-y-11 '>
-            <BiSolidShoppingBag className='w-6 h-6 icon'/>
-            <div className='px-[6px] rounded rounded-full z-40 grid place-items-center bg-[var(--primary-color)] text-white rounded rounded-[4px] text-[#f45f3f] text-bold'><b>4</b></div>
+            <BsHeart className='w-6 h-6 icon' />
+            <div className='px-[6px] rounded-full z-40 grid place-items-center bg-[var(--primary-color)] text-white text-bold'><b>4</b></div>
+          </div>
+          <div className='flex flex-col items-center -space-y-11 '>
+            <BiSolidShoppingBag className='w-6 h-6 icon' />
+            <div className='px-[6px] rounded-full z-40 grid place-items-center bg-[var(--primary-color)] text-white text-bold'><b>4</b></div>
           </div>
         </div>
-        
+
       </div>
-      
-     
+
+
     </div>
   )
 }
