ae77ddc31158ae2f324c8beddf8341775808bc83
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-08 12:08:10 +0300

    creating layout for all categories

---
+++ app/(categories)/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategoriesLayout = ({ children }) => {
+    return (
+        <div className='h-screen flex justify-center items-center'>{children}</div>
+    )
+}
+
+export default CategoriesLayout
