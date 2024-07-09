ef72b19ed4cd8016758af9863a09b2acc270cb11
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 15:53:46 +0300

    creating dedicated cart

--- app/categories/cart/page.jsx
+++ app/categories/cart/page.jsx
@@ -1,8 +1,17 @@
+'use client'
+import Categories from '@/app/_components/Categories';
+import { useCartContext } from '@/app/_components/providers/CartProvider'
 import React from 'react'
 
+
 const Cart = () => {
+    const { cartItems } = useCartContext();
     return (
-        <div>Cart</div>
+        <div className='flex flex-col'>
+            <h1 className='text-center my-2'>Here are your CART Items</h1>
+            <Categories categories={cartItems} />
+        </div>
+
     )
 }
 
