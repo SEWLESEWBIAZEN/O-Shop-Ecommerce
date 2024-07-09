2c23a34f2442a233ee5889a0888e615e293dd51c
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 10:51:22 +0300

    formatting

--- app/categories/layout.jsx
+++ app/categories/layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const CategoriesLayout = ({children}) => {
+
+const CategoriesLayout = ({ children }) => {
   return (
     <div>
-        {children }
+      {children}
     </div>
   )
 }
