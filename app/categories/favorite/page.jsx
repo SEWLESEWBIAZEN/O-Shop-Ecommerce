c899e115e97585dc5496f0b926882542361a7984
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 15:54:06 +0300

    creating dedicated favorite page

--- app/categories/favorite/page.jsx
+++ app/categories/favorite/page.jsx
@@ -1,8 +1,17 @@
+
+'use client'
+import Categories from '@/app/_components/Categories';
+import { useFavoriteContext } from '@/app/_components/providers/CartProvider'
 import React from 'react'
 
+
 const FavoriteCategories = () => {
+    const { favoriteItems } = useFavoriteContext();
     return (
-        <div>FavoriteCategories</div>
+        <div className='flex flex-col'>
+            <h1 className='text-center my-2'>Here are your FAVORITE Categories</h1>
+            <Categories categories={favoriteItems} />
+        </div>
     )
 }
 
