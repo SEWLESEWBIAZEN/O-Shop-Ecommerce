13163bcc7bb27991b1a2554f6cf2ce61b8ee198d
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 11:17:10 +0300

    accessing filtering context from hot offers page

--- app/(hot-offers)/hot-offers/page.jsx
+++ app/(hot-offers)/hot-offers/page.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Categories from '../../_components/Categories'
 import { categoriesWithDiscount } from '../../_data/categories'
+import { useFilteringContext } from '../../_components/providers/CategoriesProvider'
 
 const HotOffers = () => {
+    const { text } = useFilteringContext();
+    const [filteredCategories, setFilteredCategories] = useState(categoriesWithDiscount);
+
+    useEffect(() => {
+        const newFilteredCategories = categoriesWithDiscount.filter(category =>
+            category.title.toLowerCase().includes(text.toLowerCase()) ||
+            category.desc.toLowerCase().includes(text.toLowerCase())
+        );
+        setFilteredCategories(newFilteredCategories);
+    }, [text])
+
     return (
         <div className='py-5'>
-            <Categories categories={categoriesWithDiscount} />
+            <Categories categories={filteredCategories} />
         </div>
     )
 }
