d541766001c3b6b5f586741e1668b08aaa8ae54a
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 11:58:45 +0300

    adding favorite icon to the category component card

--- app/_components/CategoryCard.jsx
+++ app/_components/CategoryCard.jsx
@@ -2,14 +2,16 @@
 
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { AiOutlineArrowRight, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
-import { FaThumbsUp } from 'react-icons/fa'
+import { AiOutlineArrowRight, AiOutlinePlus, AiOutlineMinus, AiOutlineHeart } from 'react-icons/ai'
+import { FaThumbsUp, FaHeart } from 'react-icons/fa'
 import { useCartContext } from '../_components/providers/CartProvider'
 
 
 const CategoryCard = ({ id, img, title, desc, fav, priceHint, discount }) => {
-  const { cartAmount, setCartAmount } = useCartContext();
+  const { setCartAmount } = useCartContext();
   const [amount, setAmount] = useState(0);
+
+  const [favorite, setFavorite] = useState(false);
 
 
 
@@ -68,48 +70,60 @@ const CategoryCard = ({ id, img, title, desc, fav, priceHint, discount }) => {
   }
   return (
     <div className='h-auto'>
-      <div className={`h-full gap-1 bg-gray-50 hover:bg-white border border-gray-200 rounded-2xl ${(id % 2 !== 0) ? "sm:flex-row" : "sm:flex-row-reverse"}  flex flex-col justify-evenly items-center mx-auto p-1`}>
-        <div className='flex flex-col -space-y-12 '>
-          {discount &&
-            <div className='w-[100px] mt-5 ps-5 font-semibold text-white -rotate-45 bg-[var(--primary-color)] -px-10'>{discount}% OFF</div>}
+      <div className={`h-full gap-1 bg-gray-50 hover:bg-white border border-gray-200 rounded-2xl    mx-auto p-2 `}>
+        <div className={`${(id % 2 !== 0) ? "sm:flex-row" : "sm:flex-row-reverse"} flex flex-col justify-evenly items-center`}>
 
-          <Image
-            src={img}
-            width={160}
-            height={160}
-            alt='Category image'
-            className='rounded-2xl mx-auto sm:mx-0 cursor-pointer object-cover'
-          />
+          <div className='flex flex-col -space-y-12 '>
 
-        </div>
+            {discount &&
+              <div className='w-[100px] mt-5 ps-5 font-semibold text-white -rotate-45 bg-[var(--primary-color)] -px-10'>{discount}% OFF</div>}
 
 
-        <div className='flex flex-col gap-3 justify-center px-2'>
-          <h1 className=' text-[var(--primary-color)]  font-semibold font-mono text-md text-sm'>{title}</h1>
-          <p className='text-gray-500 max-w-[300px] text-center md:text-start text-wrap'>{desc}</p>
-          <div className='flex flex-row  text-gray-500 items-center justify-between flex-wrap'>
-            <div className='flex flex-row -space-x-1 gap-1 '>
-              {fav <= 15 && (<span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap '>{renderLikes()}</span>)}
-              {(fav > 15 && fav <= 30) && (
-                <span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap'>{render5Likes()}.......</span>
-              )
-              }
-              {fav > 30 && (
-                <span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap'>{render5Likes()}.......{render3Likes()}</span>
-              )
-              }
-
-            </div>
-            <p className='text-[12px]'>(<span>{fav}</span>)</p>
-          </div>
-          <div className='flex flex-row justify-between'>
-            <p className='text-gray-500 text-opacity-65 tracking-widest'>{priceHint}</p>
-            <AiOutlineArrowRight
-              size={25}
-              className=' text-gray-400 hover:text-[var(--primary-color)] cursor-pointer font-bold'
+            <Image
+              src={img}
+              width={160}
+              height={160}
+              alt='Category image'
+              className='rounded-2xl mx-auto sm:mx-0 cursor-pointer object-cover'
             />
+
+
+
+
           </div>
-          <div className='flex flex-row items-center justify-between'>
+
+
+          <div className='flex flex-col gap-3 justify-center px-2'>
+            <h1 className=' text-[var(--primary-color)]  font-semibold font-mono text-md text-sm'>{title}</h1>
+            <p className='text-gray-500 max-w-[300px] text-center md:text-start text-wrap'>{desc}</p>
+            <div className='flex flex-row  text-gray-500 items-center justify-between flex-wrap'>
+              <div className='flex flex-row -space-x-1 gap-1 '>
+                {fav <= 15 && (<span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap '>{renderLikes()}</span>)}
+                {(fav > 15 && fav <= 30) && (
+                  <span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap'>{render5Likes()}.......</span>
+                )
+                }
+                {fav > 30 && (
+                  <span className='flex flex-row items-center gap-1 max-w-[150px] flex-wrap'>{render5Likes()}.......{render3Likes()}</span>
+                )
+                }
+
+              </div>
+              <p className='text-[12px]'>(<span>{fav}</span>)</p>
+            </div>
+            <div className='flex flex-row justify-between'>
+              <p className='text-gray-500 text-opacity-65 tracking-widest'>{priceHint}</p>
+              <AiOutlineArrowRight
+                size={25}
+                className=' text-gray-400 hover:text-[var(--primary-color)] cursor-pointer font-bold'
+              />
+            </div>
+
+
+          </div>
+        </div>
+        <div className={`flex ${(id % 2 === 0) ? "flex-row" : "flex-row-reverse"} items-center justify-between mt-3 w-[100%] px-3`}>
+          <div className='flex flex-row justify-between items-center gap-3'>
             <button
               onClick={handleAddToCart}
               className='bg-blue-600 py-2 px-4 text-white rounded-md hover:bg-blue-500'>Add to Cart</button>
@@ -118,11 +132,17 @@ const CategoryCard = ({ id, img, title, desc, fav, priceHint, discount }) => {
               <div id='amount' name='amount' className='w-12 border text-center border-gray-300 h-6'>{amount}</div>
               <button onClick={increment} ><AiOutlinePlus size={25} /></button>
             </div>
-
+          </div>
+          <div className='cursor-pointer'
+            onClick={() => setFavorite(!favorite)}>
+            {favorite ? <FaHeart size={30} color='var(--primary-color)' /> : <AiOutlineHeart size={30} />}
           </div>
 
         </div>
       </div>
+
+
+
     </div>
 
   )
