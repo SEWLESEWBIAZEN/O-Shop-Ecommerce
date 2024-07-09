3eea8c726a766d65db68ca638a2c84313fa9748c
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-09 11:04:17 +0300

    re-rendering from the component, not from the provider as it is general for others too.

--- app/_components/providers/CategoriesProvider.jsx
+++ app/_components/providers/CategoriesProvider.jsx
@@ -1,28 +1,16 @@
 'use client';
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { categories } from '../../_data/categories';
+
 
 
 // Initialize context with default values
-const FilteringTextContext = createContext({
-    text: "",
-    setText: () => { }
-});
+const FilteringTextContext = createContext();
 
 export const CategoriesProvider = ({ children }) => {
     const [text, setText] = useState("");
-    const [filteredCategories, setFilteredCategories] = useState(categories);
-
-    useEffect(() => {
-        const newFilteredCategories = categories.filter(category =>
-            category.title.toLowerCase().includes(text.toLowerCase()) ||
-            category.desc.toLowerCase().includes(text.toLowerCase())
-        );
-        setFilteredCategories(newFilteredCategories);
-    }, [text]);
 
     return (
-        <FilteringTextContext.Provider value={{ text, setText, filteredCategories }}>
+        <FilteringTextContext.Provider value={{ text, setText }}>
             {children}
         </FilteringTextContext.Provider>
     );
